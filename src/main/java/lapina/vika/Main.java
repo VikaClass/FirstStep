ba7d7d9d380ed7458bc447671ac4f60aa2e4f2c9
VikaClass <vykpan@outlook.com>
2022-06-04 23:30:48 +0300

    Initial commit

---
+++ src/main/java/lapina/vika/Main.java
@@ -0,0 +1,16 @@
+package lapina.vika;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Main {
+    public static void main(String[] args) {
+        open("https://www.google.com/");
+        SelenideElement googleSearch = $(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        googleSearch.setValue("Котики");
+        googleSearch.pressEnter();
+    }
+}
